c13079f1725a625b9fe18c4175d66e1b0f863675
neuqs90 <e404user@gmail.com>
2024-05-02 14:28:22 +0530

    Conditions added for jokes related command

--- terminal.py
+++ terminal.py
@@ -1,8 +1,8 @@
 import commands
 
-print("\n\n---------------------------------------------------------------------")
-print("|                   Personal Terminal : NEUQS90                     |")
-print("---------------------------------------------------------------------\n")
+print("\n\n\t\t\t\t---------------------------------------------------------")
+print("\t\t\t\t|              Personal Terminal : NEUQS90              |")
+print("\t\t\t\t---------------------------------------------------------\n")
 
 while True:
     
@@ -17,9 +17,29 @@ while True:
 
             commands.get_weather_data(command_lst[1])
 
+        elif command_lst[0] in ["joke","jokes"]:
+
+            if command_lst[1].isdigit():
+
+                commands.get_jokes(command_lst[1])
+            else:
+
+                print("OOPS Master ! Please Enter Joke Count Number Afte 'joke' or 'jokes' in command")
+        else:
+
+            print("OOPS Master ! Invalid Command , PLease Check And Re-enter Command.")
+
     elif len(command_lst) == 1:
 
         if command_lst[0] in ["weather","w"]:
 
             commands.get_weather_data()
+
+        elif command_lst[0] in ["joke","jokes"]:
+
+            commands.get_jokes()
+        
+        else:
+
+            print("OOPS Master ! Invalid Command , PLease Check And Re-enter Command.")
             
