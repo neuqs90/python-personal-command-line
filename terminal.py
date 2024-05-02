68cca92d2f5995f805afb10f6ea7c9c5b0d07fc2
neuqs90 <e404user@gmail.com>
2024-05-02 07:50:43 +0530

    Conditions And Commands Check Added For Weather Data

--- terminal.py
+++ terminal.py
@@ -1,6 +1,25 @@
+import commands
+
 print("\n\n---------------------------------------------------------------------")
 print("|                   Personal Terminal : NEUQS90                     |")
-print("---------------------------------------------------------------------\n\n")
+print("---------------------------------------------------------------------\n")
 
 while True:
-    pass
+    
+    user_input = input("\n_ : ") 
+
+    command_lst = user_input.split(" ")
+
+    
+    if len(command_lst) == 2:
+
+        if command_lst[0] in ["weather","w"]:
+
+            commands.get_weather_data(command_lst[1])
+
+    elif len(command_lst) == 1:
+
+        if command_lst[0] in ["weather","w"]:
+
+            commands.get_weather_data()
+            
